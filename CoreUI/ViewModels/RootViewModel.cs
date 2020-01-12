ffb66c6c6915b29f37695fd8c7bab1c40100683a
Simon <simonica@gmail.com>
2020-01-12 15:26:25 +0000

    On à du contenu

--- CoreUI/ViewModels/RootViewModel.cs
+++ CoreUI/ViewModels/RootViewModel.cs
@@ -7,5 +7,13 @@ namespace CoreUI.ViewModels
 {
 	public class RootViewModel : Screen
 	{
+		public string Welcome { get; set; }
+
+		public RootViewModel()
+		{
+			Welcome =
+				"Eventually this application will do something very exciting indeed. " +
+				"For now, enjoy reading this pleasant text.";
+		}
 	}
 }
